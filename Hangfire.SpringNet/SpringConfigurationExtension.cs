5cad70d32969619a6aeed95818ae95e8ee87eba4
Imran Momin <360iam@gmail.com>
2016-09-11 16:41:40 -0400

    implemented the SpringJobActivator class.

---
+++ Hangfire.SpringNet/SpringConfigurationExtension.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+using Hangfire;
+using Spring.Context;
+using Spring.Context.Support;
+
+namespace ProjectiveGraph.Core.Hangfire
+{
+    public static class GlobalConfigurationExtensions
+    {
+        public static IGlobalConfiguration<SpringJobActivator> UseSpringActivator(this IGlobalConfiguration configuration, IApplicationContext context)
+        {
+            if (configuration == null) throw new ArgumentNullException("configuration");
+            if (context == null) throw new ArgumentNullException("context");
+
+            return configuration.UseActivator(new SpringJobActivator(context));
+        }
+
+        public static IGlobalConfiguration<SpringJobActivator> UseSpringActivator(this IGlobalConfiguration configuration)
+        {
+            if (configuration == null) throw new ArgumentNullException("configuration");
+
+            IApplicationContext context = ContextRegistry.GetContext();
+            if (context == null) throw new ArgumentNullException("context");
+
+            return configuration.UseActivator(new SpringJobActivator(context));
+        }
+    }
+}
---
+++ Hangfire.SpringNet/SpringJobActivator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+using Spring.Context;
+
+namespace Hangfire
+{
+    /// <summary>
+    /// HangFire Job Activator based on Spring.Net IoC Container.
+    /// </summary>
+    public sealed class SpringJobActivator : JobActivator
+    {
+        private readonly IApplicationContext context;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SpringJobActivator"/>
+        /// class with a given Spring.Net Context.
+        /// </summary>
+        /// <param name="context">Context that will be used to create instance
+        /// of classes during job activation process.</param>
+        public SpringJobActivator(IApplicationContext context)
+        {
+            if (context == null) throw new ArgumentNullException("context");
+            this.context = context;
+        }
+
+        /// <inheritdoc />
+        public override object ActivateJob(Type jobType)
+        {
+            IDictionary<string, object> objects = context.GetObjectsOfType(jobType);
+            if (objects.Count > 0) return objects.Select(v => v.Value).FirstOrDefault();
+            return null;
+        }
+    }
+}
